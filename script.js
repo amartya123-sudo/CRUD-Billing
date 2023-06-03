e074ab73cc0f588b39b3be5f2c85929419c3965f
Amartya Saran <amartyasaran@gmail.com>
2023-06-03 12:28:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// let today = new Date();
+// let dd = today.getDate();
+// let mm = today.getMonth() + 1;
+// let yyyy = today.getFullYear();
+// if (dd < 10) {
+//     dd = '0' + dd;
+// }
+// if (mm < 10) {
+//     mm = '0' + mm;
+// }
+// const date = dd + '/' + mm + '/' + yyyy;
+// document.getElementById("date").value = date;
+// console.log(date);
+
